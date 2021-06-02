fbdc205bea1b1ea2eaee3a8564b3d75974424e59
mat-czap <czapranski.mateusz@gmail.com>
2021-06-02 14:54:55 +0200

    add built version .next

---
+++ .next/static/u8BFwM2bZs5MCqRYzKjaX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea6f1f42cebc7b8cbe53.css","static/chunks/pages/index-be4938ec6c900e4674cd.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
